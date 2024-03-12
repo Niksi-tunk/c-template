2f4d10d69db463ab3243f373b7f282387ff5a611
Matias Zwinger <matias.zwinger@protonmail.com>
2024-03-12 16:39:13 +0200

    fix: wrong packages

--- flake.nix
+++ flake.nix
@@ -1,20 +1,26 @@
-
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     devenv.url = "github:cachix/devenv";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
         inputs.devenv.flakeModule
       ];
-      systems = [ "x86_64-linux" ];
+      systems = ["x86_64-linux"];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
         devenv.shells.default = {
-          packages = with pkgs; [ stdenv cmake ];
+          packages = with pkgs; [gcc gnumake];
         };
       };
     };
